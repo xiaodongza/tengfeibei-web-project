391f6b773b0c812a5962f6a815d14fd1eb0100aa
xiaodongza <398186800@qq.com>
2021-11-06 16:39:49 +0800

    first commit

---
+++ src/main/java/com/xjtu/tfbdemo/TfbApplication.java
@@ -0,0 +1,11 @@
+package com.xjtu.tfbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfbApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TfbApplication.class);
+    }
+}
---
+++ src/main/java/com/xjtu/tfbdemo/controller/QuickController.java
@@ -0,0 +1,15 @@
+package com.xjtu.tfbdemo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class QuickController {
+    @RequestMapping("/quick")
+    @ResponseBody
+    public String quick(){
+        return "hello gongxiangjiao";
+    }
+}
